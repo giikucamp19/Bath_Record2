ccbeb972d00a810fe004432a9f819a7475a7b590
orenouserNamehaKawamura1 <ta.kawamura.sys22@morijyobi.ac.jp>
2024-12-21 15:43:51 +0900

    アカウント名とアイコンを取得する処理を作成

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+// import { useEffect } from "react";
+// import axios from "axios";
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  // アカウント名
+  // const name = useRef();
+  // アカウントアイコン
+  // const icon = useRef();
+
+  // バックエンドからアカウント名とアイコンを取得する処理
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await axios.get("/api/endpoint");
+  //     name.current = response.data.name;
+  //     icon.current = response.data.icon;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);s
+  return <div>Header</div>;
+};
